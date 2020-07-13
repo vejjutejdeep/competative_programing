a69d53ceed5b68cdbfa19161f870da73c73ed6cf
vejjutejdeep <tej2544@msitprogram.net>
2020-07-13 17:14:10 +0530

    Jul 13  17:14:10

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -9,12 +9,12 @@ import math
 def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
     # your code goes here
     intersect = True
-    distance = math.ceil(math.sqrt((x2 - x1) ** 2) + (y2 - y1) ** 2)
+    distance = math.ceil(math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2)))
     if (r1 + r2) < distance:
         intersect = False
     else:
         intersect = True
 
-    print("distance " + distance)
-    print("radius " + (r1 + r2))
+    print("distance ", distance)
+    print("radius ", (r1 + r2))
     return intersect
