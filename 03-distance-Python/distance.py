4d6176f2fefda54b95b1e57ccd7047a0cdeaf8d3
vejjutejdeep <tej2544@msitprogram.net>
2020-07-13 16:43:06 +0530

    Jul 13  16:43:06

--- 03-distance-Python/distance.py
+++ 03-distance-Python/distance.py
@@ -7,4 +7,5 @@ import math
 
 def fun_distance(x1, y1, x2, y2):
     # your code goes here
-    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    print(round(math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)))
+    return round(math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
