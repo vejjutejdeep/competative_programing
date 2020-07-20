ce0604316feaf73792556158861b4afbbc258d7a
vejjutejdeep <tej2544@msitprogram.net>
2020-07-20 15:23:33 +0530

    Jul 20  15:23:33

--- 07-digitcount-Python/digitcount.py
+++ 07-digitcount-Python/digitcount.py
@@ -1,10 +1,22 @@
-# digitCount(n)[10pts]   
+# digitCount(n)[10pts]
 # Write the function digitCount(n) that takes a possibly-negative int and returns the number of digits in it.
-# So, digitCount(12323) returns 5, digitCount(0) returns 1, and digitCount(-111) returns 3. One way you 
-# could do this would be to return len(str(abs(n))), but you cannot do that, since you may not use strings 
-# here! This can be solved with logarithms, but seeing as this is "loops week", you should instead simply 
+# So, digitCount(12323) returns 5, digitCount(0) returns 1, and digitCount(-111) returns 3. One way you
+# could do this would be to return len(str(abs(n))), but you cannot do that, since you may not use strings
+# here! This can be solved with logarithms, but seeing as this is "loops week", you should instead simply
 # repeatedly remove the ones digit until you cannot.
 
 def digitcount(n):
-	# your code goes here
-	pass
+    # your code goes here.
+    count = 0
+    if n < 0:
+        n = -n
+        if n == 0:
+            return 1
+    while n != 0:
+        n = n // 10
+        count = count + 1
+    print(count)
+    return count
+
+
+digitcount(-24)
