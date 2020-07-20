acbf6069184e71ba2cb976a4bf388104588ccea3
vejjutejdeep <tej2544@msitprogram.net>
2020-07-20 18:04:09 +0530

    Jul 20  18:04:09

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -1,10 +1,10 @@
 # ishappynumber(n) [10 pts]
-# A happy number is a number defined by the following process: Starting with any positive integer, replace the 
-# number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will 
-# stay ), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 
+# A happy number is a number defined by the following process: Starting with any positive integer, replace the
+# number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will
+# stay ), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1
 # are happy numbers.
 
-# Write the function ishappynumber(n) which takes a possibly-negative integer and returns True if it is happy and 
+# Write the function ishappynumber(n) which takes a possibly-negative integer and returns True if it is happy and
 # False otherwise. Note that all numbers less than 1 are not happy. Here are some test assertions for you:
 # assert(ishappynumber(-7) == False)
 # assert(ishappynumber(1) == True)
@@ -13,7 +13,27 @@
 # assert(ishappynumber(98) == False)
 # assert(ishappynumber(404) == True)
 # assert(ishappynumber(405) == False)
+def solve(n, visited):
+    if n == 1:
+        print("True")
+    return True
+    if n == 4:
+        print("False")
+    return False
+    if n in visited:
+        print("False")
+        return False
+    su = 0
+    while n != 0:
+        su = su + ((n % 10) ** 2)
+    n = n // 10
+    visited.append(n)
+    solve(n, visited)
+
 
 def ishappynumber(n):
-	# your code goes here
-	pass
+    # your code goes here
+    solve(n, [])
+
+
+ishappynumber(19)
