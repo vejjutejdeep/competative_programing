e5a9f23e5c59ca95d2994313ed0534b70ad97e23
vejjutejdeep <tej2544@msitprogram.net>
2020-07-20 10:44:43 +0530

    Jul 20  10:44:42

--- 06-get_kth_digit-Python/get_kth_digit.py
+++ 06-get_kth_digit-Python/get_kth_digit.py
@@ -1,9 +1,19 @@
-# Write the function getKthDigit(n, k) that takes 
-# a possibly-negative int n and a non-negative int k, 
+# Write the function getKthDigit(n, k) that takes
+# a possibly-negative int n and a non-negative int k,
 # and returns the kth digit of n, starting from 0, counting from the right.
-# if the kth digit is not present return 0 
-
+# if the kth digit is not present return 0
 
 
 def fun_get_kth_digit(digit, k):
-	return 0
+    if(digit < 0):
+        digit = -digit
+    if (len(str(digit))):
+        return 0
+    n = digit
+    cout = 0
+    while (n > 0):
+        m = n % 10
+        if (cout == k):
+            return m
+        n = n//10
+        cout += 1
