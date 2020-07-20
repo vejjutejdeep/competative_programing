7ff169fd8d9912b28cdcf9a5076d026cf77ee32a
vejjutejdeep <tej2544@msitprogram.net>
2020-07-20 15:47:31 +0530

    Jul 20  15:47:31

--- 08-gcd-Python/gcd.py
+++ 08-gcd-Python/gcd.py
@@ -7,12 +7,14 @@
 #                  == gcd(20, 10) # 250 % 20 == 10
 #                  == gcd(10, 0) # 20 % 10 == 0
 # When we get to gcd(x,0), the answer is x. So gcd(270, 250) is 10. With this in mind, write the function gcd(x,y) that takes two positive integers x and y and returns their gcd using Euclid's gcd algorithm.
+import math
+
 
 def gcd(m, n):
     # your code goes here
-    if (n == 0):
-        return m
+    if min(m, n) == m:
+        return math.ceil(m//n)
     gcd(n, m % n)
 
 
-gcd(270, 250)
+print(gcd(2, 3))
