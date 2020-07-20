b6cbaa9875ba44719b70d594beb4e0260a4e2522
vejjutejdeep <tej2544@msitprogram.net>
2020-07-20 11:59:28 +0530

    Jul 20  11:59:28

--- 10-numberofpoolballs-Python/numberofpoolballs.py
+++ 10-numberofpoolballs-Python/numberofpoolballs.py
@@ -1,12 +1,22 @@
 # numberOfPoolBalls(rows) [10pts]
 # Pool balls are arranged in rows where the first row contains 1 pool ball and each row contains 1
 # more pool ball than the previous row. Thus, for example, 3 rows contain 6 total pool balls
-# (1+2+3) . With this in mind, write the function numberOfPoolBalls(rows) that takes a 
+# (1+2+3) . With this in mind, write the function numberOfPoolBalls(rows) that takes a
 # non-negative int value, the number of rows, and returns another int value, the number of pool
-# balls in that number  of full rows. For example, numberOfPoolBalls(3) returns 6. We will not 
-# limit our analysis to a "rack" of 15 balls. Rather, our pool table can contain an unlimited 
+# balls in that number  of full rows. For example, numberOfPoolBalls(3) returns 6. We will not
+# limit our analysis to a "rack" of 15 balls. Rather, our pool table can contain an unlimited
 # number of rows. For this problem and the next, you should research Triangular Numbers.
 
 def numberofpoolballs(rows):
-	# Your code goes here
-	pass
+    # Your code goes here.
+    count = 0
+    su = 0
+    while count <= rows:
+        su += count
+        count += 1
+    print(su)
+
+    return su
+
+
+numberofpoolballs(10)
