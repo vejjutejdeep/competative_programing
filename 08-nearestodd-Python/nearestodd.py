efb9abc777d5aed20b9d6382c35d734581b1acf7
vejjutejdeep <tej2544@msitprogram.net>
2020-07-20 10:58:47 +0530

    manual ppush

--- 08-nearestodd-Python/nearestodd.py
+++ 08-nearestodd-Python/nearestodd.py
@@ -1,11 +1,17 @@
-# Write the function nearestOdd(n) that takes an float n, 
-# and returns as an int value the nearest odd number to n. 
-# In the case of a tie, return the smaller odd value. 
+# Write the function nearestOdd(n) that takes an float n,
+# and returns as an int value the nearest odd number to n.
+# In the case of a tie, return the smaller odd value.
 # Note that the result must be an int, so nearestOdd(13.0) is the int 13, and not the float 13.0.
-
+import math
 
 
 def fun_nearestodd(n):
-	return 0
+    if (int(n) % 2 != 0):
+        return int(n)
+    if (int(n) == n):
+        print(int(math.floor(n)) - 1)
+        return int(math.floor(n) - 1)
+    return int(math.ceil(n))
 
 
+fun_nearestodd(12)
